6214e632968ea82b3a4d592af19f063dc816ef13
Boda <bodavivienviktoria@gmail.com>
2021-04-13 18:46:11 +0200

    feat: animal sheet basics are done

--- src/main/java/com/example/shelter/services/AnimalServiceImpl.java
+++ src/main/java/com/example/shelter/services/AnimalServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class AnimalServiceImpl implements AnimalService {
@@ -16,11 +17,18 @@ public class AnimalServiceImpl implements AnimalService {
     this.animalRepository = animalRepository;
   }
 
+  @Override
   public List<Animal> getAnimals(){
     return animalRepository.findAll();
   }
 
+  @Override
   public Animal saveAnimal(Animal animal){
     return animalRepository.save(animal);
+  }
+
+  @Override
+  public Animal getAnimal(UUID id) {
+    return animalRepository.findById(id).orElse(null);
   }
 }
